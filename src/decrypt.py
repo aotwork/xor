3e933e184b6d6c068faac660cbe88c89f960d21a
Andrew Taylor <aotaylor.work@gmail.com>
2026-04-25 12:28:27 -0500

    Updating command-line interface, adding ciphertext

--- src/decrypt.py
+++ src/decrypt.py
@@ -1,46 +1,50 @@
 #!/usr/bin/env python3
 
 from settings import default_key_path
-from exceptions import KeyFileError
 import parser
+import util
 import argparse
 import base64
 
 def decrypt(ciphertext, key):
-    ciphertext = base64.b64decode(ciphertext).decode('utf-8')
-    msglen = len(ciphertext)
+    bytearr = base64.b64decode(ciphertext)
+    message = ''
     keylen = len(key)
-    ciphers = list(map(lambda x: ord(x), ciphertext))
-    message = [chr(ciphers[i] ^ key[i % keylen]) for i in range(msglen)]
-    return ''.join(message)
+    start = 0
+    end = 0
+    i = 0
+    while start < len(ciphertext):
+        (n, e, d) = key[i % keylen]
+        size = util.size(n)
+        end = start + size
+        block = bytearr[start:end]
+        cipher = int.from_bytes(block, byteorder='big')
+        codepoint = util.power_mod_n(cipher, d, n)
+        message += chr(codepoint)
+        i += 1
+        start += size
+    return message
 
 def main():
-    arg_parser = argparse.ArgumentParser(prog='decrypt.py', description='Decrypt a message using the XOR algorithm')
-    group = arg_parser.add_mutually_exclusive_group(required=True)
+    argparser = argparse.ArgumentParser(prog='decrypt.py', description='Decrypt a message using the RSA algorithm')
+    group = argparser.add_mutually_exclusive_group(required=True)
     group.add_argument('ciphertext', type=str, nargs='?')
     group.add_argument('-i', '--inputfile', type=str)
-    arg_parser.add_argument('-k', '--keyfile', type=str, default=default_key_path)
-    arg_parser.add_argument('-o', '--outputfile', type=str)
-    args = arg_parser.parse_args()
-
+    argparser.add_argument('-k', '--keyfile', type=str, default=default_key_path)
+    argparser.add_argument('-o', '--outputfile', type=str)
+    args = argparser.parse_args()
     if args.inputfile:
-        with open(args.inputfile, 'r') as file:
+        with open(args.inputfile, 'rb') as file:
             ciphertext = file.read()
     else:
         ciphertext = args.ciphertext
-
-    try:
-        key = parser.parse_key(args.keyfile)
-    except KeyFileError as err:
-        print(err)
-        return
-
-    plaintext = decrypt(ciphertext, key)
+    key = parser.parse_key(args.keyfile)
+    msg = decrypt(ciphertext, key)
     if args.outputfile:
         with open(args.outputfile, 'w') as file:
-            file.write(plaintext)
+            file.write(msg)
     else:
-        print(plaintext, end='')
+        print(msg, end='')
 
 if __name__ == '__main__':
     main()
